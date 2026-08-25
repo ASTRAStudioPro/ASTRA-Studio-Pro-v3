aed38595af81380a25028562bd25b3a50832e1ad
ASTRAStudioPro <podrajatell.goat@gmail.com>
2026-08-25 22:53:08 +0300

    Delete HomeScreen.kt

--- HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.astra.studio
-
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen(open:()->Unit){
- Button(onClick=open){ Text("✦ ASTRA Studio Pro - Новый проект") }
-}
